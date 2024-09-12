ae5edf7cf38d96af42aab49d7be3e00f89f8837b
Coptopus <c0p7opu5@gmail.com>
2024-09-12 11:25:33 +0300

    Added PATCH operation

--- src/main/java/com/ebi/employeeapp/service/EmployeeService.java
+++ src/main/java/com/ebi/employeeapp/service/EmployeeService.java
@@ -6,7 +6,6 @@ import com.ebi.employeeapp.repo.EmployeeRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
