0fafe86357fc526acd76efa71516796a97e103e4
Coptopus <c0p7opu5@gmail.com>
2024-09-15 13:36:57 +0300

    Almost finished! :D

--- src/main/java/com/ebi/employeeapp/controller/EmployeeController.java
+++ src/main/java/com/ebi/employeeapp/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.ebi.employeeapp.controller;
 import com.ebi.employeeapp.exceptions.ResourceNotFoundException;
 import com.ebi.employeeapp.model.EmpSaveDTO;
 import com.ebi.employeeapp.model.EmployeeDTO;
+import com.ebi.employeeapp.repo.EmployeeRepository;
 import com.ebi.employeeapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,8 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @GetMapping
     @ResponseBody
@@ -26,6 +29,13 @@ public class EmployeeController {
         List<EmployeeDTO> employees = employeeService.getAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
+
+    @GetMapping("/home")
+    public String getHome(Model model) {
+
+        return "index";
+    }
+
 
     @GetMapping("/view")
     public String getAllEmployeesView(Model model) {
@@ -66,7 +76,7 @@ public class EmployeeController {
     }
 
     @PostMapping("/addEmployee")
-    public String postForm(@ModelAttribute("employee") EmployeeDTO employeeDTO, Model model) {
+    public String postForm(@ModelAttribute("employee") EmployeeDTO employeeDTO) {
         employeeService.createEmployee(employeeDTO);
         return "redirect:../employees/view";
     }
@@ -93,6 +103,24 @@ public class EmployeeController {
         }
     }
 
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+
+        EmployeeDTO employee = employeeService.getEmployeeById(id);
+        model.addAttribute("employee", employee);
+        return "updateEmployee";
+    }
+
+    @PostMapping("/update")
+    public String postUpdateForm(EmpSaveDTO empSaveDTO) {
+        EmpSaveDTO updatedEmployee = employeeService.patchEmployee(empSaveDTO);
+        if (updatedEmployee != null) {
+            return "redirect:../employees/view";
+        } else {
+            throw new ResourceNotFoundException("Employee with id " + empSaveDTO.getId_employee() + " not found");
+        }
+    }
+
     @DeleteMapping("/{id}")
     @ResponseBody
     public ResponseEntity<Void> deleteEmployee(@PathVariable int id) {
@@ -102,5 +130,26 @@ public class EmployeeController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEmployeeById(@PathVariable int id, Model model) {
+        model.addAttribute("employee", employeeService.getEmployeeById(id));
+        return "deleteEmployee";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteEmployeeByIdReturn(@PathVariable int id) {
+        boolean deleted = employeeService.deleteEmployee(id);
+        if (deleted) {
+            return "redirect:../view";
+        } else {
+            throw new ResourceNotFoundException("Employee with id " + id + " not found");
+        }
+    }
+
+    @GetMapping("/contact")
+    public String getContact() {
+        return "contact";
     }
 }
